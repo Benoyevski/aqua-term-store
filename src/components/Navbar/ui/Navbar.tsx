efd87a8eb4df785f2184b2dd6ab455fd4b32d1a0
”Ismail” <ibeno95@mail.ru>
2023-06-08 14:23:39 +0300

    Сделал всплывающий инпут и поиск продуктов

--- src/components/Navbar/ui/Navbar.tsx
+++ src/components/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { classNames } from "../../../shared/classNames/classNames";
 import { Logo } from "../../../shared/ui/Logo/Logo";
 import { Menu } from "../../../shared/ui/Menu/Menu";
+import { Search } from "../../Search";
 import cls from "./Navbar.module.scss";
 
 interface NavbarProps {
@@ -14,15 +15,7 @@ export const Navbar = ({ className }: NavbarProps) => {
                 <div className={cls.navbarContent}>
                     <Logo />
                     <Menu />
-                    <div className={cls.search}>
-                        <div className={cls.searchWrap}>
-                            <img
-                                className={cls.searchIcon}
-                                src='src/shared/assets/search.png'
-                                alt='lupa'
-                            />
-                        </div>
-                    </div>
+                    <Search />
                 </div>
             </div>
         </header>
---
+++ src/components/Search/ui/Search.tsx
@@ -0,0 +1,83 @@
+import { ChangeEvent, FC, useEffect } from "react";
+import { useState } from "react";
+import { classNames } from "../../../shared/classNames/classNames";
+import cls from "./Search.module.scss";
+import { useAppDispatch, useAppSelector } from "../../../app/utils/hooks";
+import { fetchProducts } from "../../../features/productSlice";
+import { IProduct } from "../../../app/types/types";
+
+interface SearchProps {
+    className?: string;
+}
+
+export const Search: FC = ({ className }: SearchProps) => {
+    const dispatch = useAppDispatch();
+    const [inputActive, setInputActive] = useState<boolean>(false);
+    const [inputValue, setInputValue] = useState<string>("");
+
+    const products: IProduct[] = useAppSelector((state) => state.product.items);
+    const searchedProducts = products.filter(
+        (el) => inputValue.length > 2 && el.name.toLowerCase().includes(inputValue.toLowerCase()),
+    );
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, []);
+
+    const handleInputActive = () => {
+        setInputActive(!inputActive);
+    };
+
+    const handleCloseInput = () => {
+        setInputActive(false);
+        setInputValue("");
+    };
+
+    const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+
+    return (
+        <div className={classNames(cls.Search, {}, [className])}>
+            <div className={cls.searchWrap}>
+                <img
+                    onClick={handleInputActive}
+                    className={cls.searchIcon}
+                    src='src/shared/assets/search.png'
+                    alt='lupa'
+                />
+            </div>
+            <div className={inputActive ? cls.SearchInputDropdownActive : cls.SearchInputDropdown}>
+                <div className={cls.container}>
+                    <div className={cls.inputWrapper}>
+                        <input
+                            value={inputValue}
+                            onChange={handleChangeInput}
+                            placeholder='Поиск'
+                            type='text'
+                        />
+
+                        <span onClick={handleCloseInput} className={cls.closeInputBtn}>
+                            X
+                        </span>
+                    </div>
+                </div>
+                <div
+                    className={classNames(cls.searchItemList, { [cls.hidden]: !inputActive }, [
+                        className,
+                    ])}
+                >
+                    {searchedProducts?.map((el) => (
+                        <div className={cls.searchItemWrapper}>
+                            <div className={cls.container}>
+                                <div className={cls.searchItem} key={el._id}>
+                                    {el.name}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
