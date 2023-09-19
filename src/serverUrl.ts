d9299b0ee5cfbdf1a63c47db42955a5f0459fca8
”Ismail” <ibeno95@mail.ru>
2023-09-19 13:59:41 +0300

    fix server url

--- src/serverUrl.ts
+++ src/serverUrl.ts
@@ -5,6 +5,6 @@ export let serverUrl: string;
 if (NODE_ENV === "development") {
     serverUrl = "http://localhost:5000/"; // адрес сервера на локалке
 } else {
-    // serverUrl = "https://aquaterm-store-backend.onrender.com/";
-    serverUrl = "http://localhost:5000/";
+    serverUrl = "https://aquaterm-store-backend.onrender.com/";
+    // serverUrl = "http://localhost:5000/";
 }
